9e70c9d6c7ce8a86af4545298527cb70e2581a2d
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-05-10 18:02:05 +0200

    NRP-254 Declare java type for xs:time in the bindings file. Change package name for netex to no.rutebanken.netex.model

--- src/test/java/MarshalUnmarshalTest.java
+++ src/test/java/MarshalUnmarshalTest.java
@@ -1,11 +1,16 @@
+import no.rutebanken.netex.model.ArrivalStructure;
+import no.rutebanken.netex.model.MultilingualString;
+import no.rutebanken.netex.model.ObjectFactory;
+import no.rutebanken.netex.model.PublicationDeliveryStructure;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.xml.sax.InputSource;
-import uk.org.netex.netex.*;
-import uk.org.siri.siri.ParticipantRefStructure;
 
 
 import javax.xml.bind.*;
+import javax.xml.namespace.QName;
 import java.io.*;
+import java.time.LocalTime;
 import java.time.ZonedDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -39,6 +44,30 @@ public class MarshalUnmarshalTest {
         assertThat(actual.getDescription()).isNotNull();
         assertThat(actual.getDescription().getValue()).isEqualTo(publicationDelivery.getDescription().getValue());
         assertThat(actual.getParticipantRef()).isEqualTo(publicationDelivery.getParticipantRef());
+    }
+
+    @Ignore
+    @Test
+    public void localTime() throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(PublicationDeliveryStructure.class);
+        Marshaller marshaller = jaxbContext.createMarshaller();
+
+        ArrivalStructure arrival = new ArrivalStructure()
+                .withTime(LocalTime.NOON);
+
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        marshaller.marshal(new JAXBElement<>(new QName("http://www.netex.org.uk/netex", "Arrival"), ArrivalStructure.class, null, arrival), byteArrayOutputStream);
+
+        String xml = byteArrayOutputStream.toString();
+        System.out.println(xml);
+
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+        JAXBElement<ArrivalStructure> jaxbElement = (JAXBElement<ArrivalStructure>) unmarshaller.unmarshal(new ByteArrayInputStream(xml.getBytes()));
+        ArrivalStructure actual = jaxbElement.getValue();
+
+        assertThat(actual.getTime()).hasSameHourAs(arrival.getTime());
     }
 
 }
