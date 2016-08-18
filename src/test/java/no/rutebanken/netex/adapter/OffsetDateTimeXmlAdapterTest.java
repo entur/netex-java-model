196e8422328c3502928990d439b2c96e4205a526
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-08-18 09:22:38 +0200

    NRP-545 Test timezone offset when marshalling/unmarshalling

--- src/test/java/no/rutebanken/netex/adapter/OffsetDateTimeXmlAdapterTest.java
+++ src/test/java/no/rutebanken/netex/adapter/OffsetDateTimeXmlAdapterTest.java
@@ -2,10 +2,7 @@ package no.rutebanken.netex.adapter;
 
 import org.junit.Test;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
+import java.time.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,17 +11,26 @@ public class OffsetDateTimeXmlAdapterTest {
     OffsetDateTimeXmlAdapter offsetDateTimeXmlAdapter = new OffsetDateTimeXmlAdapter();
 
     @Test
-    public void unmarshal() throws Exception {
-        // The dateTime is specified in the following form "YYYY-MM-DDThh:mm:ss"
+    public void unmarshalUTC() throws Exception {
         OffsetDateTime actual = offsetDateTimeXmlAdapter.unmarshal("2016-08-17T04:26:23Z");
         assertThat(actual.getDayOfMonth()).isEqualTo(17);
+        assertThat(actual.getMonthValue()).isEqualTo(8);
+        assertThat(actual.getHour()).isEqualTo(4);
+        assertThat(actual.getMinute()).isEqualTo(26);
+        assertThat(actual.getSecond()).isEqualTo(23);
+        assertThat(actual.getOffset().toString()).isEqualTo("Z");
+    }
+
+    @Test
+    public void unmarshalUTCMinus6Hours() throws Exception {
+        OffsetDateTime actual = offsetDateTimeXmlAdapter.unmarshal("2016-08-17T04:26:23-06:00");
+        assertThat(actual.getOffset().toString()).isEqualTo("-06:00");
     }
 
     @Test
     public void marshal() throws Exception {
         OffsetDateTime offsetDateTime = OffsetDateTime.ofInstant(Instant.ofEpochMilli(1000000000000L), ZoneId.of("Z"));
         String actual = offsetDateTimeXmlAdapter.marshal(offsetDateTime);
-        System.out.println(actual);
         assertThat(actual).isEqualTo("2001-09-09T01:46:40Z");
     }
 
