d4ccd7670cb38358c8f736445db0d22f6e86fccc
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-11-29 13:52:15 +0100

    NRP-772 Add successful validation test as well

--- src/test/java/org/rutebanken/netex/validation/NeTExValidatorTest.java
+++ src/test/java/org/rutebanken/netex/validation/NeTExValidatorTest.java
@@ -12,17 +12,28 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NeTExValidatorTest {
 
+    private NeTExValidator neTExValidator = new NeTExValidator();
+    
+    public NeTExValidatorTest() throws IOException, SAXException {    }
+
     @Test
     public void validationFailsForInvalidXml() throws SAXException, IOException {
-
-        NeTExValidator neTExValidator = new NeTExValidator();
-
         String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
                 "<PublicationDelivery xmlns=\"http://www.netex.org.uk/netex\" xmlns:ns2=\"http://www.opengis.net/gml/3.2\" xmlns:ns3=\"http://www.siri.org.uk/siri\"></PublicationDelivery>";
 
         assertThatThrownBy(() -> {
             neTExValidator.validate(new StreamSource(new StringReader(xml)));
         }).isInstanceOf(SAXParseException.class);
+    }
 
+    @Test
+    public void validatePublicationDelivery() throws IOException, SAXException {
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                "<PublicationDelivery xmlns=\"http://www.netex.org.uk/netex\" xmlns:ns2=\"http://www.opengis.net/gml/3.2\" xmlns:ns3=\"http://www.siri.org.uk/siri\" version=\"any\">\n" +
+                "    <PublicationTimestamp>2016-11-29T13:32:06.869+01:00</PublicationTimestamp>\n" +
+                "    <ParticipantRef>NSR</ParticipantRef>\n" +
+                "</PublicationDelivery>";
+
+        neTExValidator.validate(new StreamSource(new StringReader(xml)));
     }
 }
