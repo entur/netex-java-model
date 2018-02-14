ebf82c2b29d20261895bee1bc2c20e18e28c34f5
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-02-14 15:27:23 +0100

    ROR-97 Make script for downloading and extracting netex versions a bit more robust. Add suppport for zip path to extract, and destination folder so it can be used with multiple versions

--- bin/netex-download-extract.sh
+++ bin/netex-download-extract.sh
@@ -6,33 +6,51 @@
 : ${NETEX_REPO:="NeTEx"}
 : ${GITHUB_URL:="https://github.com/entur/$NETEX_REPO"}
 : ${GIT_BRANCH:="master"}
-: ${GIT_BRANCH:="master"}
-: ${PROFILE_PATH:="./profile"}
+: ${DESTINATION_PATH:="./profile"}
+: ${ZIP_PATH_TO_EXTRACT:="${NETEX_REPO}-${GIT_BRANCH}/xsd/*"}
 
-echo "NETEX full github URL: $GITHUB_URL"
+echo "NETEX repo github URL: $GITHUB_URL Github branch: $GIT_BRANCH"
 
-echo "Removing any existing contents in $PROFILE_PATH"
-rm -rf ${PROFILE_PATH}/*
+echo "Removing any existing contents in $DESTINATION_PATH"
+rm -rf ${DESTINATION_PATH}/*
+mkdir -p ${DESTINATION_PATH}
 
 ZIPFILE="${GIT_BRANCH}.zip"
 
 if [ -f ${ZIPFILE} ]; then
-  echo "Removing existin file $ZIPFILE"
+  echo "Removing existing file $ZIPFILE"
   rm ${ZIPFILE}
 fi
 
 WGET_URL="${GITHUB_URL}/archive/${ZIPFILE}"
+echo "About to download from $WGET_URL"
 wget -q ${WGET_URL}
 
 if [ -f ${ZIPFILE} ]; then
-    echo "Unzip to ${PROFILE_PATH}"
-    unzip ${ZIPFILE} "${NETEX_REPO}-${GIT_BRANCH}/xsd/*" -d ${PROFILE_PATH}
-    rm ${ZIPFILE}
-    REMOVE_FOLDER="${NETEX_REPO}-${GIT_BRANCH}"
-    echo "Remove intermediate folder ${PROFILE_PATH}/${REMOVE_FOLDER}"
-    mv "${PROFILE_PATH}/${REMOVE_FOLDER}/xsd" ${PROFILE_PATH}
-    rm -rf "${PROFILE_PATH}/${REMOVE_FOLDER}"
-    echo "Profile extracted to $PROFILE_PATH"
+    echo "Done"
+    {
+    echo "Create ${DESTINATION_PATH}" &&
+    mkdir -p ${DESTINATION_PATH} &&
+
+    echo "Unzip to ${DESTINATION_PATH}" &&
+    unzip -q ${ZIPFILE} ${ZIP_PATH_TO_EXTRACT} -d ${DESTINATION_PATH} &&
+
+    echo "Remove zipfile ${ZIPFILE}" &&
+    rm ${ZIPFILE} &&
+
+    REMOVE_FOLDER=${ZIP_PATH_TO_EXTRACT} &&
+
+    echo "Remove intermediate folder ${REMOVE_FOLDER}" &&
+
+    mv ${DESTINATION_PATH}/${REMOVE_FOLDER} ${DESTINATION_PATH} &&
+    rm -rf "${DESTINATION_PATH}/${REMOVE_FOLDER}" &&
+
+    echo "Profile extracted to $DESTINATION_PATH"
+    } ||
+    {
+        (>&2 echo "Error extracting zip file $ZIPFILE from $WGET_URL. See my previous output for details")
+        exit 1
+    }
 else
     (>&2 echo "Error downloading zip file $ZIPFILE from $WGET_URL")
     exit 1
