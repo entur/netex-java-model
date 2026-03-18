2786511bd68b1459e0843356c384a6a5be13d37c
Leonard Ehrenfried <mail@leonard.io>
2026-03-18 16:04:41 +0100

    Deduplicate 'any' version attribute

---
+++ src/main/java/org/rutebanken/util/VersionXmlAdapter.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed under the EUPL, Version 1.2 or - as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ *   https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+
+package org.rutebanken.util;
+
+import jakarta.xml.bind.annotation.adapters.XmlAdapter;
+import java.util.HashMap;
+
+public class VersionXmlAdapter extends XmlAdapter<String, String> {
+
+	/**
+	 * EPIP allows the string 'any' as a valid version, so we deduplicate it to conserve memory. In one
+	 * especially bad example this saved 1.7 GB of memory!
+	 */
+	private static final String ANY = "any";
+
+	@Override
+	public String unmarshal(String input) {
+		if(input.equals(ANY)) {
+			return ANY;
+		}
+		else {
+			return input;
+		}
+	}
+
+	@Override
+	public String marshal(String input) {
+		return input;
+	}
+
+}
