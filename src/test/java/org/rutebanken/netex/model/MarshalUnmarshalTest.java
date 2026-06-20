ecb867caf0db14f1d656385ee5b85227bed9103e
Stefan Jugelt <stefan@MacBook-Pro-2.local>
2026-06-20 15:47:30 +0200

    Timestamp with nanoseconds is accepted

--- src/test/java/org/rutebanken/netex/model/MarshalUnmarshalTest.java
+++ src/test/java/org/rutebanken/netex/model/MarshalUnmarshalTest.java
@@ -434,4 +434,54 @@ class MarshalUnmarshalTest {
 		assertFalse(xml.contains(netexNamespace));
 	}
 
+		@Test
+	void unmarshalPublicationDeliveryAndVerifyDateTimeNanoSeconds() throws JAXBException {
+
+		String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+				+ "<PublicationDelivery version=\"1.0\" xmlns=\"http://www.netex.org.uk/netex\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.netex.org.uk/netex ../../xsd/NeTEx_publication.xsd\">"
+				+ " <PublicationTimestamp>2016-05-18T15:00:00.0123456+01:00</PublicationTimestamp>"
+				+ " <ParticipantRef>NHR</ParticipantRef>"
+				+ " <dataObjects>"
+				+ "  <SiteFrame version=\"01\" id=\"nhr:sf:1\">"
+				+ "   <stopPlaces>"
+				+ "    <StopPlace version=\"01\" created=\"2016-04-21T09:00:00.01234568Z\" id=\"nhr:sp:1\">"
+				+ "     <Centroid>"
+				+ "      <Location srsName=\"WGS84\">"
+				+ "       <Longitude>10.8577903</Longitude>"
+				+ "       <Latitude>59.910579</Latitude>"
+				+ "      </Location>"
+				+ "     </Centroid>"
+				+ "     <Name lang=\"no-NO\">Krokstien</Name>    "
+				+ "     <TransportMode>bus</TransportMode>"
+				+ "     <StopPlaceType>onstreetBus</StopPlaceType>"
+				+ "     <quays>"
+				+ "      <Quay version=\"01\" created=\"2016-04-21T09:01:00.01234569Z\" id=\"nhr:sp:1:q:1\">"
+				+ "       <Centroid>"
+				+ "        <Location srsName=\"WGS84\">"
+				+ "         <Longitude>10.8577903</Longitude>"
+				+ "         <Latitude>59.910579</Latitude>"
+				+ "        </Location>"
+				+ "       </Centroid>"
+				+ "       <Covered>outdoors</Covered>"
+				+ "       <Lighting>wellLit</Lighting>"
+				+ "       <QuayType>busStop</QuayType>"
+				+ "      </Quay>"
+				+ "     </quays>"
+				+ "    </StopPlace>"
+				+ "   </stopPlaces>"
+				+ "  </SiteFrame>"
+				+ " </dataObjects>"
+				+ "</PublicationDelivery>";
+
+		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+		@SuppressWarnings("unchecked")
+		JAXBElement<PublicationDeliveryStructure> jaxbElement = (JAXBElement<PublicationDeliveryStructure>) unmarshaller
+				.unmarshal(new ByteArrayInputStream(xml.getBytes()));
+		PublicationDeliveryStructure actual = jaxbElement.getValue();
+
+		System.out.println(actual.getPublicationTimestamp());
+		assertThat(actual.getPublicationTimestamp().getHour()).isEqualTo(15);
+	}
+
 }
